03baf3f002bd9ccb213711347c0a863e7036a2a8
Oybek Baxtiyorov <programmeroybek211@gmail.com>
2024-04-17 17:01:20 +0500

    fiiid

--- src/components/home/save-modal.jsx
+++ src/components/home/save-modal.jsx
@@ -47,9 +47,9 @@ export default function SaveModal() {
                     className="relative text-lg font-medium leading-6 flex justify-center items-center"
                   >
                     <div className="flex gap-[10px] flex-col w-full pb-[20px]">
-                      <h1 className="text-[#333] text-[15px] font-[500] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px] opacity-[0.5]">📍Lokatsiyani yuborish</h1>
-                      <h1 className="text-[#333] text-[15px] font-[500] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px]">❤️ Sevimlilar</h1>
-                      <h1 className="text-[#333] text-[15px] font-[500] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px] opacity-[0.5]">🔙 Ortga</h1>
+                      <h1 className="text-[#333] text-[15px] font-[400] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px] opacity-[0.5]">📍Lokatsiyani yuborish</h1>
+                      <h1 className="text-[#333] text-[15px] font-[400] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px]">❤️ Sevimlilar</h1>
+                      <h1 className="text-[#333] text-[15px] font-[400] bg-[#d8d8d8] w-full text-center py-[9px] rounded-[4px] opacity-[0.5]">🔙 Ortga</h1>
                     </div>
                   </Dialog.Title>
                   <div className="w-full flex justify-center flex-col gap-[12px] mt-[24px]">
