34f07065501ca62cbad6a2f7a5f95df5d91d2937
Oybek Baxtiyorov <programmeroybek211@gmail.com>
2024-04-25 15:12:21 +0500

    fd

--- src/image-blur/discount-item-blur.jsx
+++ src/image-blur/discount-item-blur.jsx
@@ -14,7 +14,7 @@ export const DiscountItemBlur = ({ src }) => {
     }, [src]);
 
     return (
-        <div className='mx-auto rounded-[8px] w-[90%] h-[230px] overflow-hidden'>
+        <div className='mx-auto rounded-[8px] w-[95%] h-[230px] overflow-hidden'>
             <div
                 style={{
                     display: imgLoading ? 'none' : 'block',
@@ -24,7 +24,7 @@ export const DiscountItemBlur = ({ src }) => {
             >
                 <Blurhash
                     hash='LEHV6nWB2yk8pyo0adR*.7kCMdnj'
-                    width={"90%"}
+                    width={"95%"}
                     height={230}
                     resolutionX={32}
                     resolutionY={32}
@@ -33,7 +33,7 @@ export const DiscountItemBlur = ({ src }) => {
             </div>
             <img
                 style={{ display: imgLoading ? 'block' : 'none' }}
-                className=" inline-flex rounded-[8px] w-[90%]   object-cover"
+                className=" inline-flex rounded-[8px] w-[95%]   object-cover"
                 src={src ? src : baseUrl}
                 alt={'img'}
             />
